97856b8444b519ede4d7f9474f059c34ef083746
yukikimoto <ktc2243020002@edu.kyoto-tech.ac.jp>
2024-07-22 18:34:04 +0900

    envファイルから環境変数を読み込むコードの追加

--- Program.cs
+++ Program.cs
@@ -1,62 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 using DotNetEnv;
 
-using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
+Console.WriteLine("Hello, World!");
 
 // .envファイルを読み込む
 Env.Load();
 
-// APIを利用するための設定
-var builder = Kernel.CreateBuilder()
-    .AddAzureOpenAIChatCompletion(
-        deploymentName: Env.GetString("YOUR_MODEL_ID"),
-        endpoint: Env.GetString("YOUR_ENDPOINT_NAME"),
-        apiKey: Env.GetString("API_KEY")
-        );
+// .envファイルの内容を取得
+string apiKey = Env.GetString("TEST_KEY");
 
-
-
-// カーネルをビルド
-Kernel kernel = builder.Build();
-var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-
-// Toolを指定
-OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
-{
-    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
-};
-
-// チャット履歴を作成
-var history = new ChatHistory();
-
-// ユーザーの入力を受け取り、それに対する応答を生成
-string? userInput;
-do
-{
-    Console.Write("User > ");
-    // ユーザーの入力を受け取る
-    userInput = Console.ReadLine();
-
-    // ユーザーの入力が空の場合は、次の入力を待つ
-    if (string.IsNullOrEmpty(userInput))
-    {
-        continue;
-    }
-
-    // チャット履歴にユーザーのメッセージを追加
-    history.AddUserMessage(userInput);
-
-    // チャット履歴を使用して、エージェントのメッセージを取得
-    var result = await chatCompletionService.GetChatMessageContentAsync(
-        history,
-        executionSettings: openAIPromptExecutionSettings,
-        kernel: kernel);
-
-    // エージェントのメッセージを表示
-    Console.WriteLine("Assistant > " + result);
-
-    // チャット履歴にエージェントのメッセージを追加
-    history.AddMessage(result.Role, result.Content ?? string.Empty);
-} while (userInput is not null);
+Console.WriteLine("apikey : " + apiKey);
